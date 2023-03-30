e7d551f598ccfe848a4cb4682d05d9268e997f4c
NeuralNuts <wilsonzakk8@gmail.com>
2023-03-30 11:11:10 +1000

    misc

--- Program.cs
+++ Program.cs
@@ -6,14 +6,12 @@ using System.Reflection;
 #endregion
 
 #region Instanciate {builder} Variable From WebApplication Class
-var builder =
-WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 #endregion
 
 #region Add Services To The Container 
 builder.Services.Configure<MongoDBSettings>(
-builder.Configuration.GetSection(
-"MongoDB"));
+builder.Configuration.GetSection("MongoDB"));
 
 builder.Services.AddSingleton<MongoDBServices>();
 builder.Services.AddControllers().AddNewtonsoftJson();
@@ -24,22 +22,15 @@ builder.Services.AddSwaggerGen(c =>
     "v1",
     new OpenApiInfo
     {
-        Version = 
-        "v1.0",
-        Title =
-        "Sensor Data API",
-        Description =
-        @"An edcucational api for interfacing with sensor data
-        from IOT devices using mongoDB and C#.."
+        Version = "v1.0",
+        Title = "Sensor Data API",
+        Description = @"An edcucational api for interfacing with sensor data
+                        from IOT devices using mongoDB and C#.."
     });
 
-    var xmlFile =
-    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 
-    var xmlPath =
-    Path.Combine(
-    AppContext.BaseDirectory,
-    xmlFile);
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
     c.IncludeXmlComments(xmlPath);
 });
@@ -51,21 +42,20 @@ options =>
 {
     options.AddDefaultPolicy(
     builder =>
-        {
+    {
             builder.WithOrigins(
             "https://localhost:44351",
             "https://localhost:4200",
             "https://www.google.com/")
             .AllowAnyHeader()
             .AllowAnyMethod();
-        });
+    });
 });
 
 #endregion
 
 #region Instanciate {app} Variable With Build() From {builder} variable
-var app =
-builder.Build();
+var app = builder.Build();
 #endregion
 
 #region Configure The HTTP Request Pipeline & Control When Swagger Is In Use
--- Services/MongoDBServices.cs
+++ Services/MongoDBServices.cs
@@ -22,27 +22,19 @@ namespace CLIMATE_REST_API.Services
         public MongoDBServices(
         IOptions<MongoDBSettings> mongodbSettings)
         {
-            MongoClient client = 
-            new MongoClient(
-            mongodbSettings
+            MongoClient client = new MongoClient(mongodbSettings
             .Value
             .ConnectionURI);
 
-            IMongoDatabase database = 
-            client.GetDatabase(
-            mongodbSettings
+            IMongoDatabase database = client.GetDatabase(mongodbSettings
             .Value
             .Database);
 
-            _weatherCollection = 
-            database.GetCollection<SensorDataModel>(
-            mongodbSettings
+            _weatherCollection = database.GetCollection<SensorDataModel>(mongodbSettings
             .Value
             .Collection1);
 
-            _userCollection = 
-            database.GetCollection<UserModel>(
-            mongodbSettings
+            _userCollection = database.GetCollection<UserModel>(mongodbSettings
             .Value
             .Collection2);
         }
@@ -54,8 +46,7 @@ namespace CLIMATE_REST_API.Services
         public async Task<List<SensorDataModel>> GetAllSensorReadingsAsync()
         {
             return await _weatherCollection
-            .Find(
-            new BsonDocument()).
+            .Find(new BsonDocument()).
             Limit(100).
             ToListAsync();
         }
@@ -65,67 +56,47 @@ namespace CLIMATE_REST_API.Services
         public async Task<SensorDataModel> GetSensorByIdAsync(
         string id)
         {
-            var id_filter =
-            Builders<SensorDataModel>
+            var id_filter = Builders<SensorDataModel>
             .Filter
-            .Eq(
-            "Id",
-            id);
+            .Eq(u => u.Id, id);
 
             return await _weatherCollection
-            .Find(
-            id_filter)
+            .Find(id_filter)
             .FirstAsync();
         }
         #endregion
 
         #region Gets The Max Precipitation Of A Device And DateTime Range
-        public async Task<string> GetMaxPrecipitaionAsync(
-        string device)
+        public async Task<string> GetMaxPrecipitaionAsync(string device)
         {
-            var device_filter =
-            Builders<SensorDataModel>
+            var device_filter = Builders<SensorDataModel>
             .Filter
-            .Eq(
-            "Device Name",
-            device);
+            .Eq(u => u.Device, device);
 
-            var time_filter =
-            Builders<SensorDataModel>
+            var time_filter = Builders<SensorDataModel>
             .Filter
-            .Lt(
-            "Time",
-            DateTime
+            .Lt(u => u.Time, DateTime
             .Now
             .AddMonths(-5));
 
-            var project_stage =
-            Builders<SensorDataModel>
+            var project_stage = Builders<SensorDataModel>
             .Projection
             .Expression(
-            u =>
-            new
+            u => new
             {
-                Device =
-                u.Device,
-                Precipitation_mm_h = 
-                u.Precipitation_mm_h,
-                Time =
-                u.Time
+                Device = u.Device,
+                Precipitation_mm_h = u.Precipitation_mm_h,
+                Time = u.Time
             });
 
-            var result =
-                await _weatherCollection
+            var result = await _weatherCollection
                 .Aggregate().
-                Match(
-                device_filter).
-                Match(
-                time_filter).
+                Match(device_filter).
+                Match(time_filter).
                 SortByDescending(
                 u => 
                 u.Precipitation_mm_h).
-                Project(
-                project_stage).
+                Project(project_stage).
                 FirstAsync();
 
             return result.ToJson();
@@ -133,54 +104,34 @@ namespace CLIMATE_REST_API.Services
         #endregion
 
         #region Gets Readings Based On A DateTime Range And A Device
-        public async Task<string> GetFieldsBasedOnTimeAndDateAsync(
-        string device,
-        DateTime date_time)
+        public async Task<string> GetFieldsBasedOnTimeAndDateAsync(string device, DateTime date_time)
         {
-            var device_filter =
-            Builders<SensorDataModel>
+            var device_filter = Builders<SensorDataModel>
             .Filter
-            .Eq(
-            "Device Name",
-            device);
+            .Eq(u => u.Device, device);
 
-            var time_filter = 
-            Builders<SensorDataModel>
+            var time_filter = Builders<SensorDataModel>
             .Filter
-            .Eq(
-            "Time",
-            date_time);
+            .Eq(u => u.Time, date_time);
 
-            var project_stage =
-            Builders<SensorDataModel>
+            var project_stage = Builders<SensorDataModel>
             .Projection
             .Expression(
-            u =>
-            new
+            u => new
             {
-                Device =
-                u.Device,
-                Precipitation_mm_h =
-                u.Precipitation_mm_h,
-                Time =
-                u.Time,
-                AtmosphericPressure_kPa =
-                u.AtmosphericPressure_kPa,
-                Temperature_C =
-                u.Temperature_C,
-                SolarRadiation_Wm2 =
-                u.SolarRadiation_Wm2
+                Device = u.Device,
+                Precipitation_mm_h = u.Precipitation_mm_h,
+                Time = u.Time,
+                AtmosphericPressure_kPa = u.AtmosphericPressure_kPa,
+                Temperature_C = u.Temperature_C,
+                SolarRadiation_Wm2 = u.SolarRadiation_Wm2
             });
 
-            var result =
-                await _weatherCollection
+            var result = await _weatherCollection
                 .Aggregate().
-                Match(
-                time_filter).
-                Match(
-                device_filter).
-                Project(
-                project_stage).
+                Match(time_filter).
+                Match(device_filter).
+                Project(project_stage).
                 FirstAsync();
 
             return result.ToJson();
@@ -188,56 +139,37 @@ namespace CLIMATE_REST_API.Services
         #endregion
 
         #region Gets The Max Tempreture Based On A Start Date And A End Date
-        public async Task<string> GetMaxTempAsync(
-        DateTime date_time_start,
-        DateTime date_time_end)
+        public async Task<string> GetMaxTempAsync(DateTime date_time_start, DateTime date_time_end)
         {
-            var filter_1 =
-            Builders<SensorDataModel>
+            var filter_1 = Builders<SensorDataModel>
             .Filter
-            .Gt(
-            "Time",
-            date_time_start);
+            .Gt(u => u.Time, date_time_start);
 
-            var filter_2 = 
-            Builders<SensorDataModel>
+            var filter_2 = Builders<SensorDataModel>
             .Filter
-            .Lt(
-            "Time",
-            date_time_end);
+            .Lt(u => u.Time, date_time_end);
 
-            var filters =
-            Builders<SensorDataModel>
+            var filters = Builders<SensorDataModel>
             .Filter
-            .And(
-            filter_1,
-            filter_2);
+            .And(filter_1, filter_2);
 
-            var project_stage =
-            Builders<SensorDataModel>
+            var project_stage = Builders<SensorDataModel>
             .Projection
             .Expression(
-            u =>
-            new
+            u => new
             {
-                Device =
-                u.Device,
-                Time =
-                u.Time,
-                Temperature_C =
-                u.Temperature_C,
+                Device = u.Device,
+                Time = u.Time,
+                Temperature_C = u.Temperature_C,
             });
 
-            var result =
-                await _weatherCollection.
+            var result = await _weatherCollection.
                 Aggregate().
-                Match(
-                filters).
+                Match(filters).
                 SortByDescending(
                 u => 
                 u.Temperature_C).
-                Project(
-                project_stage).
+                Project(project_stage).
                 Limit(10).
                 ToListAsync();
 
@@ -246,53 +178,39 @@ namespace CLIMATE_REST_API.Services
         #endregion
 
         #region Creates A New Sesnor Reading
-        public async Task CreateWeatherAsync(
-        SensorDataModel weather)
+        public async Task CreateWeatherAsync(SensorDataModel weather)
         {
             await _weatherCollection
-            .InsertOneAsync(
-            weather);
+            .InsertOneAsync(weather);
 
             return;
         }
         #endregion
 
         #region Creates Manny Sensor Readings 
-        public async Task CreateMannyWeatherAsync(
-        List<SensorDataModel> weather)
+        public async Task CreateMannyWeatherAsync(List<SensorDataModel> weather)
         { 
 
             await _weatherCollection
-            .InsertManyAsync(
-            weather);
+            .InsertManyAsync(weather);
 
             return;
         }
         #endregion
 
         #region Updates The Precipitation For A Single Device
-        public async Task UpdatePrecipitaionAsync(
-        string id,
-        double precipitation_mm_h)
+        public async Task UpdatePrecipitaionAsync(string id, double precipitation_mm_h)
         {
-            FilterDefinition<SensorDataModel> filter =
-            Builders<SensorDataModel>
+            FilterDefinition<SensorDataModel> filter = Builders<SensorDataModel>
             .Filter
-            .Eq(
-            "Id",
-            id);
+            .Eq(u => u.Id, id);
 
-            UpdateDefinition<SensorDataModel> update =
-            Builders<SensorDataModel>
+            UpdateDefinition<SensorDataModel> update = Builders<SensorDataModel>
             .Update
-            .Set(
-            "Precipitation mm/h",
-            precipitation_mm_h);
+            .Set(u => u.Precipitation_mm_h, precipitation_mm_h);
 
             await _weatherCollection
-            .UpdateOneAsync(
-            filter,
-            update);
+            .UpdateOneAsync(filter, update);
 
             return;
         }
@@ -306,36 +224,23 @@ namespace CLIMATE_REST_API.Services
         public async Task<List<UserModel>> GetUserAsync()
         {
             return await _userCollection
-            .Find(
-            new BsonDocument())
+            .Find(new BsonDocument())
            .Limit(10)
            .ToListAsync();
         }
         #endregion
 
         #region Authenticates A User
-        public async Task<UserModel?> AuthenticateUserAsync(
-        string api_token,
-        string role)
+        public async Task<UserModel?> AuthenticateUserAsync(string api_token, string role)
         {
-            var api_token_filter =
-            Builders<UserModel>
+            var api_token_filter = Builders<UserModel>
             .Filter
-            .Eq(
-            u => 
-            u.ApiToken, 
-            api_token);
+            .Eq(u => u.ApiToken, api_token);
 
-            var user =
-            await _userCollection.Find(
-            api_token_filter)
+            var user = await _userCollection.Find(api_token_filter)
             .FirstOrDefaultAsync();
 
-            if (
-            user ==
-            null ||
-            user.Role !=
-            role)
+            if (user == null || user.Role != role)
             {
                 return null;
             }
@@ -345,60 +250,42 @@ namespace CLIMATE_REST_API.Services
         #endregion
 
         #region Creates A New User
-        public async Task<bool> CreatedUserAsync(
-        UserModel user_model)
+        public async Task<bool> CreatedUserAsync(UserModel user_model)
         {
-            var filter = 
-            Builders<UserModel>
+            var filter = Builders<UserModel>
             .Filter
-            .Eq(
-            u => 
-            u.UserEmail, 
-            user_model.UserEmail);
+            .Eq(u => u.UserEmail, user_model.UserEmail);
 
-            var existing_user = 
-            await _userCollection
-            .Find(
-            filter)
+            var existing_user = await _userCollection
+            .Find(filter)
             .FirstOrDefaultAsync();
 
-            if (
-            existing_user != 
-            null)
+            if (existing_user != null)
             {
                 return false;
             }
 
-            user_model.ApiToken = 
-            Guid
+            user_model.ApiToken = Guid
             .NewGuid()
             .ToString();
 
-            user_model.CreatedDate =
-            DateTime.Now;
+            user_model.CreatedDate = DateTime.Now;
 
             await _userCollection
-            .InsertOneAsync(
-            user_model);
+            .InsertOneAsync(user_model);
 
             return true;
         }
         #endregion
 
         #region Deletes A User
-        public async Task DeleteUserByIdAsync(
-        string api_token,
-        string id)
+        public async Task DeleteUserByIdAsync(string api_token, string id)
         {
-            FilterDefinition<UserModel> filter = 
-            Builders<UserModel>
+            FilterDefinition<UserModel> filter = Builders<UserModel>
             .Filter
-            .Eq(
-            "Id",
-            id);
+            .Eq(u => u.Id, id);
 
-            await _userCollection.DeleteOneAsync(
-            filter);
+            await _userCollection.DeleteOneAsync(filter);
 
             return;
         }
@@ -406,134 +293,80 @@ namespace CLIMATE_REST_API.Services
 
 
         #region Deletes Many Users
-        public async Task<UserModel> DeletesManyUsersAsync(
-        string api_token,
-        DateTime date_time_start,
-        DateTime date_time_end)
+        public async Task<UserModel> DeletesManyUsersAsync(string api_token, DateTime date_time_start, DateTime date_time_end)
         {
-            var filter_date_time_start =
-            Builders<UserModel>
+            var filter_date_time_start = Builders<UserModel>
             .Filter
-            .Gte(
-            u => 
-            u.LoginDate, 
-            date_time_start);
+            .Gte(u => u.LoginDate, date_time_start);
 
-            var filter_date_time_end = 
-            Builders<UserModel>
+            var filter_date_time_end = Builders<UserModel>
             .Filter
-            .Lte(
-            u => 
-            u.LoginDate, 
-            date_time_end);
+            .Lte(u => u.LoginDate, date_time_end);
 
             var filters = Builders<UserModel>
             .Filter
-            .And(
-            filter_date_time_start,
-            filter_date_time_end);
+            .And(filter_date_time_start, filter_date_time_end);
 
-            var users = 
-            _userCollection.Find(
-            filters)
+            var users = _userCollection.Find(filters)
             .FirstOrDefault();
 
-            if (
-            users == 
-            null)
+            if (users == null)
             {
                 return null;
             }
 
-            await _userCollection.DeleteManyAsync(
-            filters);
+            await _userCollection.DeleteManyAsync(filters);
 
             return users;
         }
         #endregion
 
         #region Updates A User Login Time
-        public async Task UpdateUserLoginTimeAsync(
-        string api_token, 
-        DateTime login_date_time)
+        public async Task UpdateUserLoginTimeAsync(string api_token, DateTime login_date_time)
         {
-            var api_token_filter = 
-            Builders<UserModel>
+            var api_token_filter = Builders<UserModel>
             .Filter
-            .Eq(
-            u => 
-            u.ApiToken, 
-            api_token);
+            .Eq(u => u.ApiToken, api_token);
 
-            var update = 
-            Builders<UserModel>
+            var update = Builders<UserModel>
             .Update
-            .Set(
-            u => 
-            u.LoginDate, 
-            login_date_time);
+            .Set(u => u.LoginDate, login_date_time);
 
-            await _userCollection.UpdateOneAsync(
-            api_token_filter, 
-            update);
+            await _userCollection.UpdateOneAsync(api_token_filter, update);
 
             return;
         }
         #endregion
 
         #region Updates Many Users Roles
-        public async Task<UserModel?> PatchUsersRoleAsync(
-        string property, 
-        object value, 
-        DateTime date_time_start, 
-        DateTime date_time_end)
+        public async Task<UserModel?> PatchUsersRoleAsync(string property, object value, DateTime date_time_start, DateTime date_time_end)
         {
-            var filter_date_time_start = 
-            Builders<UserModel>
+            var filter_date_time_start = Builders<UserModel>
             .Filter
-            .Gte(
-            u => 
-            u.CreatedDate, 
-            date_time_start);
+            .Gte(u => u.CreatedDate, date_time_start);
 
-            var filter_date_time_end = 
-            Builders<UserModel>
+            var filter_date_time_end = Builders<UserModel>
             .Filter
-            .Lte(
-            u => 
-            u.CreatedDate, 
-            date_time_end);
+            .Lte(u => u.CreatedDate, date_time_end);
 
-            var filters = 
-            Builders<UserModel>
+            var filters = Builders<UserModel>
             .Filter
-            .And(
-            filter_date_time_start,
-            filter_date_time_end);
+            .And(filter_date_time_start, filter_date_time_end);
 
-            var value_property_update = 
-            Builders<UserModel>
+            var value_property_update = Builders<UserModel>
             .Update
-            .Set(
-            property,
-            value);
+            .Set(property, value);
 
-            var users =
-            _userCollection
-            .Find(
-            filters)
+            var users = _userCollection
+            .Find(filters)
             .FirstOrDefault();
 
-            if (
-            users == 
-            null)
+            if (users == null)
             {
                 return null;
             }
 
-            await _userCollection.UpdateManyAsync(
-            filters, 
-            value_property_update);
+            await _userCollection.UpdateManyAsync(filters, value_property_update);
 
             return users;
         }
